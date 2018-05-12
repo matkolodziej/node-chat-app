3da9b07d3bacc31589123d9c3cbde9b994445e83
Mat Kolodziej <mat.kolodziej@hotmail.com>
2018-05-12 13:03:20 -0600

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log('Connected to Server');
-
-    socket.emit('createMessage', {
-        from: 'Andrew',
-        text: 'Yop, that works for me.'
-    })
 });
 
 socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user connected');
 
-    socket.emit('newMessage', {
-        from: 'Jon',
-        text: 'See you then',
-        createdAt: 123
-    });
-
     socket.on('createMessage', (message) => {
         console.log('create Message', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
